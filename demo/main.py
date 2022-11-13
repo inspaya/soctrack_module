949a606e0b5c92c5fea9134d2257b6185340658b
Muizudeen Kusimo <muizz@inspaya.com>
2022-11-13 10:49:07 +0100

    Temporarily disable Redis search so we can build Maps Polygon structre

--- demo/main.py
+++ demo/main.py
@@ -24,7 +24,7 @@ EXCLUDED_REDISEARCH_XTERS.remove(".")
 RESULT_COUNT = 1000
 RESULT_RADIUS = 1500
 CACHE_EXPIRY_24HRS = 86400
-ASKNGMAP_DEFAULT_MARKER = {"tweet": "AskNGMap.Live", "lat": 8.8941, "lng": 7.1860}
+ASKNGMAP_DEFAULT_MARKER = {"text": "AskNGMap.Live", "lat": 8.8941, "lng": 7.1860}
 
 _redis_conn = from_url(environ.get("REDIS_OM_URL"))
 
@@ -64,7 +64,7 @@ class SocTrackMood(JsonModel):
     lng: float = Field(index=True)
 
     def __repr__(self):
-        return f"<Mood: {self.text} assessed as {self.sentiment}>"
+        return f"<Mood: {self.tweet} assessed as {self.sentiment}>"
 
     class Meta:
         database = _redis_conn
@@ -99,13 +99,14 @@ class TwitterBridge:
 
     @staticmethod
     async def tweets_analyzer(redis_conn: Redis, new_tweets_list: list):
-        _assigned_sentiments = analyze_batch(new_tweets_list)
-        for _tweet in _assigned_sentiments:
+        _tweets_by_text = {_tweet['text']: _tweet for _tweet in new_tweets_list}
+        _assigned_sentiments = analyze_batch(_tweets_by_text.keys())
+        for _tweet_text in _tweets_by_text.keys():
             _scored_tweet = SocTrackMood(
-                tweet=_tweet['tweet'],
-                sentiment=_tweet['sentiment'],
-                lat=_tweet['coordinates'].split(',')[0],
-                lng=_tweet['coordinates'].split(',')[1]
+                tweet=_tweets_by_text[_tweet_text]['text'],
+                sentiment=_tweets_by_text[_tweet_text]['sentiment'],
+                lat=_tweets_by_text[_tweet_text]['coordinates'].split(',')[0],
+                lng=_tweets_by_text[_tweet_text]['coordinates'].split(',')[1]
             )
 
             # redis_conn.json().set('', '$', _scored_tweet, nx=True)
@@ -137,9 +138,63 @@ async def get_search_results(customer_location: Union[str, None] = None, query: 
         query = AskNgMapEngine.sanitize_redisearch_query_str(query)
         _search_query = f' (@tweet: ( {query} ) )'
         _search_query = Query(query_string=_search_query).paging(offset=0, num=RESULT_COUNT)
-        search_results = _redis_conn.ft(index_name=REDIS_SEARCH_QUERY_INDEX).search(_search_query).docs
+        # search_results = _redis_conn.ft(index_name=REDIS_SEARCH_QUERY_INDEX).search(_search_query).docs
+        search_results = [{
+            "positive": [  # Pure +ves or (no detectable + +ve)
+                {
+                    "text": "I hope I can buy fuel today, I hear AA Rano has good prices",
+                    "lat":9.0318959,
+                    "lng": 7.482812200000001
+                },
+                {
+                    "text": "Yes! Finally refuelled my car. I was so lucky, as there were only 2 vehicles on the queue",
+                    "lat": 9.039967,
+                    "lng": 7.484536299999999
+                }
+            ],
+            "negative": [  # Pure -ves or (no detectable + -ve)
+                {
+                    "text": "Black marketers wan kill me o! Imagine N2,500 for 1 litre of fuel!",
+                    "lat": 9.0623633,
+                    "lng": 7.458546800000001
+                },
+                {
+                    "text": "Another day, another queue, hopefully we'll get fuel today.",
+                    "lat": 9.0613569,
+                    "lng": 7.421806200000001
+                },
+                {
+                    "text": "I have resolved to cycling as I can't keep up with this fuel madness",
+                    "lat": 9.0538139,
+                    "lng": 7.4778843
+                },
+                {
+                    "text": "No fuel in my car, no fuel in my gen, no fuel in my life! This country sha!",
+                    "lat": 9.039967,
+                    "lng": 7.484536299999999
+                },
+                {
+                    "text": "Today was baaaad! Been at the fuel station for over 3hrs and yet no hope to buy fuel :<",
+                    "lat": 9.2855902,
+                    "lng": 7.378668900000001
+                },
+            ],
+            "neutral": [  # Pure no detectable
+                {
+                    "text": "This fuel sef, when will we stop suffering from queues",
+                    "lat": 8.645035199999999,
+                    "lng": 10.7718025
+                }
+            ]
+        }]
 
-        if len(search_results) > 0:
+        result_count = [
+            len(search_results[0]['positive']),
+            len(search_results[0]['negative']),
+            len(search_results[0]['neutral'])
+        ]
+
+        if any(result_count):
             return {"data": search_results}
         else:
             return {"data": [ASKNGMAP_DEFAULT_MARKER]}
@@ -172,5 +227,5 @@ async def ingest_tweet_data(new_tweets_list: list, gmaps_tweet_plotter: Backgrou
 
 @app.on_event("startup")
 async def load():
-    aioredis_conn = _redis_conn
-    FastAPICache.init(RedisBackend(aioredis_conn), prefix="askngmap_search_cache")
+    redis_conn = _redis_conn
+    FastAPICache.init(RedisBackend(redis_conn), prefix="askngmap_search_cache")
